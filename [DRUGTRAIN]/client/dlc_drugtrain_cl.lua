0009bd7930180db186b1b27921768f88aa117b05
Azeroth <76072277+Azerothwav@users.noreply.github.com>
2023-01-15 00:19:52 +0100

    Update dlc_drugtrain_cl.lua

--- [DRUGTRAIN]/client/dlc_drugtrain_cl.lua
+++ [DRUGTRAIN]/client/dlc_drugtrain_cl.lua
@@ -1,52 +1,29 @@
 local SecurityPed = {}
 local drugstrainblip = nil
-local traindrugspawn = false
 local testspawnpnj = false
 local caissedrug = {}
 
-RegisterNetEvent('az_train:dlccreateDrugsTrainClient')
-AddEventHandler('az_train:dlccreateDrugsTrainClient', function(index)
+RegisterNetEvent('az_train:dlccreateDrugsTrainClient', function(index)
     SendNotification(DrugTrainConfig.Lang["EventStart"])
-    if Config.FrameWork == 'ESX' then
-        ESX.TriggerServerCallback('az_train:dlchavespawntrain', function(spawntrain)
-            if spawntrain then
-                RequestModel(GetHashKey('v_ind_cf_boxes'))
-                while not HasModelLoaded(GetHashKey('v_ind_cf_boxes')) do
-                    Citizen.Wait(1)
-                end
-                temptraindrugs = CreateMissionTrain(0, DrugTrainConfig.DrugTrain[index].coordstrain.x, DrugTrainConfig.DrugTrain[index].coordstrain.y, DrugTrainConfig.DrugTrain[index].coordstrain.z, math.random(0,100))
-                SetTrainSpeed(temptraindrugs, 0)
-                SetTrainCruiseSpeed(temptraindrugs,0)
-                FreezeEntityPosition(temptraindrugs, true)
-                for k, v in pairs(DrugTrainConfig.Cargaison) do
-                    local coordscaisse = GetOffsetFromEntityInWorldCoords(temptraindrugs, v.x, v.y, v.z - 1.0)
-                    caissedrug[k] = CreateObject(GetHashKey('v_ind_cf_boxes'), coordscaisse.x, coordscaisse.y, coordscaisse.z, true, false, false)
-                    FreezeEntityPosition(caissedrug[k], true)
-                    SetEntityHeading(caissedrug[k], GetEntityHeading(temptraindrugs))
-                end
-                TriggerServerEvent("az_train:syncTrainEventModel", VehToNet(temptraindrugs))
+    TriggerCallback('az_train:dlchavespawntrain', function(spawntrain)
+        if spawntrain then
+            RequestModel(GetHashKey('v_ind_cf_boxes'))
+            while not HasModelLoaded(GetHashKey('v_ind_cf_boxes')) do
+                Citizen.Wait(1)
             end
-        end)
-    elseif Config.FrameWork == 'QBCore' then
-        QBCore.Functions.TriggerCallback('az_train:dlchavespawntrain', function(spawntrain)
-            if spawntrain then
-                RequestModel(GetHashKey('v_ind_cf_boxes'))
-                while not HasModelLoaded(GetHashKey('v_ind_cf_boxes')) do
-                    Citizen.Wait(1)
-                end
-                temptraindrugs = CreateMissionTrain(0, DrugTrainConfig.DrugTrain[index].coordstrain.x, DrugTrainConfig.DrugTrain[index].coordstrain.y, DrugTrainConfig.DrugTrain[index].coordstrain.z, math.random(0,100))
-                SetTrainSpeed(temptraindrugs, 0)
-                SetTrainCruiseSpeed(temptraindrugs,0)
-                FreezeEntityPosition(temptraindrugs, true)
-                for k, v in pairs(DrugTrainConfig.Cargaison) do
-                    local coordscaisse = GetOffsetFromEntityInWorldCoords(temptraindrugs, v.x, v.y, v.z - 1.0)
-                    caissedrug[k] = CreateObject(GetHashKey('v_ind_cf_boxes'), coordscaisse.x, coordscaisse.y, coordscaisse.z, true, false, false)
-                    FreezeEntityPosition(caissedrug[k], true)
-                    SetEntityHeading(caissedrug[k], GetEntityHeading(temptraindrugs))
-                end
+            temptraindrugs = CreateMissionTrain(0, DrugTrainConfig.DrugTrain[index].coordstrain.x, DrugTrainConfig.DrugTrain[index].coordstrain.y, DrugTrainConfig.DrugTrain[index].coordstrain.z, math.random(0,100))
+            SetTrainSpeed(temptraindrugs, 0)
+            SetTrainCruiseSpeed(temptraindrugs,0)
+            FreezeEntityPosition(temptraindrugs, true)
+            for k, v in pairs(DrugTrainConfig.Cargaison) do
+                local coordscaisse = GetOffsetFromEntityInWorldCoords(temptraindrugs, v.x, v.y, v.z - 1.0)
+                caissedrug[k] = CreateObject(GetHashKey('v_ind_cf_boxes'), coordscaisse.x, coordscaisse.y, coordscaisse.z, true, false, false)
+                FreezeEntityPosition(caissedrug[k], true)
+                SetEntityHeading(caissedrug[k], GetEntityHeading(temptraindrugs))
             end
-        end)
-    end
+            TriggerServerEvent("az_train:syncTrainEventModel", VehToNet(temptraindrugs))
+        end
+    end)
     ShowBlipToEveryone(DrugTrainConfig.DrugTrain[index].coordstrain, DrugTrainConfig.DrugTrain[index].showblip)
     indrugmissiontrain = true
     LaunchTrain(index)
@@ -56,8 +33,14 @@ RegisterNetEvent("az_train:syncTrainEventModel", function(netid)
     temptraindrugs = NetToVeh(netid)
 end)
 
-RegisterNetEvent('az_train:dlcremoveDrugsTrainClient')
-AddEventHandler('az_train:dlcremoveDrugsTrainClient', function(index)
+RegisterNetEvent("az_train:syncPNJTrainEvent", function(pnjdata)
+    for k, v in pairs(pnjdata) do
+        SecurityPed[k] = NetToPed(v)
+        print(SecurityPed[k])
+    end
+end)
+
+RegisterNetEvent('az_train:dlcremoveDrugsTrainClient', function(index)
     SendNotification(DrugTrainConfig.Lang["EventStop"])
     for b, c in pairs(SecurityPed) do
         DeleteEntity(c)
@@ -124,81 +107,47 @@ function LaunchTrain(index)
                     end
                     if not testspawnpnj then
                         testspawnpnj = true
-                        if Config.FrameWork == 'ESX' then
-                            ESX.TriggerServerCallback('az_train:dlchavespawnpnj', function(spawnpnj)
-                                if spawnpnj then
-                                    for b, n in pairs(SecurityPed) do
-                                        SecurityPed[b] = nil
-                                    end
-                                    AddRelationshipGroup('pedennemiedrug')
-                                    for x, w in pairs(DrugTrainConfig.DrugTrain[index].security) do
-                                        RequestModel(GetHashKey(w.ped))
-                                        while not HasModelLoaded(GetHashKey(w.ped)) do
-                                            Citizen.Wait(0)
-                                        end
-                                        SecurityPed[x] = CreatePed(7,GetHashKey(w.ped),w.coordspnj.x, w.coordspnj.y, w.coordspnj.z - 1, math.random(0, 120),true,true)
-                                        if w.scenario ~= nil then
-                                            TaskStartScenarioInPlace(SecurityPed[x], w.scenario, 0, false)
-                                        end
-                                        if w.health < 200 then
-                                            SetPedMaxHealth(SecurityPed[x], w.health)
-                                        end
-                                        if w.armor > 0 then
-                                            SetPedArmour(SecurityPed[x], w.armor)
-                                        end
-                                        if w.weapon ~= nil then
-                                            GiveWeaponToPed(SecurityPed[x], GetHashKey(w.weapon), 250, false, true)
-                                            SetCurrentPedWeapon(SecurityPed[x], GetHashKey(w.weapon), true)
-                                            SetPedAccuracy(SecurityPed[x], w.accuracy)
-                                        end
-                                        if w.freeze then
-                                            FreezeEntityPosition(SecurityPed[x], true)
-                                        end
-                                        SetPedCombatRange(SecurityPed[x], 2)
-                                        SetPedCombatAttributes(SecurityPed[x], 46, true)
-                                        SetPedRelationshipGroupHash(SecurityPed[x], GetHashKey('pedennemiedrug'))
-                                        SetRelationshipBetweenGroups(5, GetHashKey('pedennemiedrug'), GetHashKey("PLAYER"))
-                                    end
+                        TriggerCallback('az_train:dlchavespawnpnj', function(spawnpnj)
+                            if spawnpnj then
+                                for b, n in pairs(SecurityPed) do
+                                    SecurityPed[b] = nil
                                 end
-                            end)
-                        elseif Config.FrameWork == 'QBCore' then
-                            QBCore.Functions.TriggerCallback('az_train:dlchavespawnpnj', function(spawnpnj)
-                                if spawnpnj then
-                                    for b, n in pairs(SecurityPed) do
-                                        SecurityPed[b] = nil
+                                AddRelationshipGroup('pedennemiedrug')
+                                for x, w in pairs(DrugTrainConfig.DrugTrain[index].security) do
+                                    RequestModel(GetHashKey(w.ped))
+                                    while not HasModelLoaded(GetHashKey(w.ped)) do
+                                        Citizen.Wait(0)
                                     end
-                                    AddRelationshipGroup('pedennemiedrug')
-                                    for x, w in pairs(DrugTrainConfig.DrugTrain[index].security) do
-                                        RequestModel(GetHashKey(w.ped))
-                                        while not HasModelLoaded(GetHashKey(w.ped)) do
-                                            Citizen.Wait(0)
-                                        end
-                                        SecurityPed[x] = CreatePed(7,GetHashKey(w.ped),w.coordspnj.x, w.coordspnj.y, w.coordspnj.z - 1, math.random(0, 120),true,true)
-                                        if w.scenario ~= nil then
-                                            TaskStartScenarioInPlace(SecurityPed[x], w.scenario, 0, false)
-                                        end
-                                        if w.health < 200 then
-                                            SetPedMaxHealth(SecurityPed[x], w.health)
-                                        end
-                                        if w.armor > 0 then
-                                            SetPedArmour(SecurityPed[x], w.armor)
-                                        end
-                                        if w.weapon ~= nil then
-                                            GiveWeaponToPed(SecurityPed[x], GetHashKey(w.weapon), 250, false, true)
-                                            SetCurrentPedWeapon(SecurityPed[x], GetHashKey(w.weapon), true)
-                                            SetPedAccuracy(SecurityPed[x], w.accuracy)
-                                        end
-                                        if w.freeze then
-                                            FreezeEntityPosition(SecurityPed[x], true)
-                                        end
-                                        SetPedCombatRange(SecurityPed[x], 2)
-                                        SetPedCombatAttributes(SecurityPed[x], 46, true)
-                                        SetPedRelationshipGroupHash(SecurityPed[x], GetHashKey('pedennemiedrug'))
-                                        SetRelationshipBetweenGroups(5, GetHashKey('pedennemiedrug'), GetHashKey("PLAYER"))
+                                    SecurityPed[x] = CreatePed(7,GetHashKey(w.ped),w.coordspnj.x, w.coordspnj.y, w.coordspnj.z - 1, math.random(0, 120),true,true)
+                                    if w.scenario ~= nil then
+                                        TaskStartScenarioInPlace(SecurityPed[x], w.scenario, 0, false)
                                     end
+                                    if w.health < 200 then
+                                        SetPedMaxHealth(SecurityPed[x], w.health)
+                                    end
+                                    if w.armor > 0 then
+                                        SetPedArmour(SecurityPed[x], w.armor)
+                                    end
+                                    if w.weapon ~= nil then
+                                        GiveWeaponToPed(SecurityPed[x], GetHashKey(w.weapon), 250, false, true)
+                                        SetCurrentPedWeapon(SecurityPed[x], GetHashKey(w.weapon), true)
+                                        SetPedAccuracy(SecurityPed[x], w.accuracy)
+                                    end
+                                    if w.freeze then
+                                        FreezeEntityPosition(SecurityPed[x], true)
+                                    end
+                                    SetPedCombatRange(SecurityPed[x], 2)
+                                    SetPedCombatAttributes(SecurityPed[x], 46, true)
+                                    SetPedRelationshipGroupHash(SecurityPed[x], GetHashKey('pedennemiedrug'))
+                                    SetRelationshipBetweenGroups(5, GetHashKey('pedennemiedrug'), GetHashKey("PLAYER"))
                                 end
-                            end)
-                        end
+                                local serverTableNetID = {}
+                                for x, w in pairs(SecurityPed) do
+                                    serverTableNetID[x] = PedToNet(w)
+                                end
+                                TriggerServerEvent("az_train:syncPNJTrainEvent", serverTableNetID)
+                            end
+                        end)
                     end
                 end
             end
@@ -211,6 +160,18 @@ function RemoveThisTrain(traintodelete)
     Citizen.SetTimeout(30000, function()
         DeleteMissionTrain(traintodelete)
     end)
+end
+
+function TriggerCallback(callbackname, cb, ...)
+    if Config.FrameWork == 'ESX' then
+        ESX.TriggerServerCallback(callbackname, function(tempcb)
+            cb(tempcb)
+        end, ...)
+    elseif Config.FrameWork == 'QBCore' then
+        QBCore.Functions.TriggerCallback(callbackname, function(tempcb)
+            cb(tempcb)
+        end, ...)
+    end
 end
 
 print("[AZ_TRAIN] - DLC Drug Train Load")
